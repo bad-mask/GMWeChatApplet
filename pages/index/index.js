3dcccf28b8ad1c22192290108c7638d7030a6e46
bad-mask <zhangzhangliyin@gmail.com>
2018-03-12 16:26:57 +0800

    调列表 UI

--- pages/index/index.js
+++ pages/index/index.js
@@ -32,7 +32,84 @@ Page({
         value: 'stars',
         groups: ['003'],
       }
-    ]
+    ],
+    type: `grid`,
+    components: [{
+      title: '【近视矫正】同仁主任亲自操刀，还你清晰美好“视界”',
+      url: '/pages/actionsheet/index',
+      service_img:'http://hera.s.igengmei.com/service/2015/06/25/671cb7b167-half',
+    },
+    {
+      title: '【瘦脸针】小脸自拍无需耍心机，靠前也自信！',
+      url: '/pages/backdrop/index',
+      service_img:'http://hera.s.igengmei.com/service/2015/04/23/5e3d19b169-half',
+    },
+    {
+      title: '【自体脂肪面部填充】脸颊嘭嘭~嘭起来~',
+      url: '/pages/barcode/index',
+      service_img:'http://hera.s.igengmei.com/service/2015/03/16/5edc65de0a-half',
+    },
+    {
+      title: '【射频除皱】午餐美容，快速解决松皱！',
+      url: '/pages/button/index',
+      service_img:'http://hera.s.igengmei.com/service/2015/07/15/10db5af8d1-half',
+    },
+    {
+      title: '【假体隆下巴】勾勒精致轮廓，塑造俏皮挺立下巴！',
+      url: '/pages/calendar/index',
+      service_img:'http://hera.s.igengmei.com/service/2015/03/31/48555431ab-half',
+    },
+    {
+      title: '【北京@连喜艳】瘦腿针（300单位）',
+      url: '/pages/countdown/index',
+      service_img:'http://hera.s.igengmei.com/service/2015/06/03/413772eccf-half',
+    },
+    {
+      title: '【北京@郑宗梅】唇部整形',
+      url: '/pages/countup/index',
+      service_img:'http://hera.s.igengmei.com/service/2015/05/04/0c546e6c3d-half',
+    },
+    {
+      title: '【北京@圣贝口腔】进口纳米树脂补牙',
+      url: '/pages/dialog/index',
+      service_img:'http://hera.s.igengmei.com/service/2015/05/13/4a5f504da6-half',
+    },
+    {
+      title: '【补牙】采用进口纳米树脂补牙材料，耐磨有光泽',
+      url: '/pages/filterbar/index',
+      service_img:'http://hera.s.igengmei.com/service/2015/03/12/93f30b1cc1-half',
+    },
+    {
+      title: '【北京@连喜艳】瘦腿针（300单位）',
+      url: '/pages/countdown/index',
+      service_img: 'http://hera.s.igengmei.com/service/2015/06/03/413772eccf-half',
+    },
+    {
+      title: '【北京@郑宗梅】唇部整形',
+      url: '/pages/countup/index',
+      service_img: 'http://hera.s.igengmei.com/service/2015/05/04/0c546e6c3d-half',
+    },
+    {
+      title: '【北京@圣贝口腔】进口纳米树脂补牙',
+      url: '/pages/dialog/index',
+      service_img: 'http://hera.s.igengmei.com/service/2015/05/13/4a5f504da6-half',
+    },
+    {
+      title: '【北京@连喜艳】瘦腿针（300单位）',
+      url: '/pages/countdown/index',
+      service_img: 'http://hera.s.igengmei.com/service/2015/06/03/413772eccf-half',
+    },
+    {
+      title: '【北京@郑宗梅】唇部整形',
+      url: '/pages/countup/index',
+      service_img: 'http://hera.s.igengmei.com/service/2015/05/04/0c546e6c3d-half',
+    },
+    {
+      title: '【北京@圣贝口腔】进口纳米树脂补牙',
+      url: '/pages/dialog/index',
+      service_img: 'http://hera.s.igengmei.com/service/2015/05/13/4a5f504da6-half',
+    },
+    ],
   },
   onLoad: function () {
     this.$filterBar = $filterBar.init({
